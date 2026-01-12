d3e25ea67ded30e690143616eac00536797fd06e
theoHC <theo_coulson@me.com>
2026-01-11 18:25:47 -0600

    Angle tests done

--- turtlelib/include/turtlelib/angle.hpp
+++ turtlelib/include/turtlelib/angle.hpp
@@ -7,6 +7,7 @@
 
 namespace turtlelib
 {
+    constexpr double pi = std::numbers::pi;
     /// \brief Approximately compare two floating-point numbers using
     ///        an absolute comparison
     /// \param d1 A number to compare
@@ -23,7 +24,7 @@ namespace turtlelib
     /// \returns The equivalent angle in radians
     constexpr double deg2rad(double deg)
     {
-        return (deg / 180) * std::numbers::pi;
+        return (deg / 180) * pi;
     }
 
     /// \brief Convert radians to degrees
@@ -31,7 +32,7 @@ namespace turtlelib
     /// \returns The equivalent angle in degrees
     constexpr double rad2deg(double rad)
     {
-        return (rad / std::numbers::pi) * 180;
+        return (rad / pi) * 180;
     }
 
     /// \brief Wrap an angle to (-PI, PI]
@@ -40,13 +41,10 @@ namespace turtlelib
     constexpr double normalize_angle(double rad)
     {
         // ####### begin_citation [5] #######
-        const double pi = std::numbers::pi;
-        const double two_pi = 2.0 * pi;
+        double r = std::fmod(rad, 2 * pi);
 
-        double r = std::fmod(rad, two_pi);
-
-        if (r <= -pi) r += two_pi;
-        if (r >  pi)  r -= two_pi;
+        if (r <= -pi) r += 2 * pi;
+        if (r >  pi)  r -= 2 * pi;
         return r;
         // ####### end_citation [5] #######
     }
@@ -60,22 +58,29 @@ namespace turtlelib
     static_assert(almost_equal(0.0, -0.0), "zeros are the same");
 
     static_assert(almost_equal(deg2rad(0.0), 0.0), "deg2rad failed");
-    static_assert(almost_equal(deg2rad(180.0), std::numbers::pi), "deg2rad failed");
-    static_assert(almost_equal(deg2rad(-180.0), -1 * std::numbers::pi), "deg2rad failed");
-    static_assert(almost_equal(deg2rad(90), 0.5 * std::numbers::pi), "deg2rad failed");
-    static_assert(almost_equal(deg2rad(270), 1.5 * std::numbers::pi), "deg2rad failed");
+    static_assert(almost_equal(deg2rad(180.0), pi), "deg2rad failed");
+    static_assert(almost_equal(deg2rad(-180.0), -1*pi), "deg2rad failed");
+    static_assert(almost_equal(deg2rad(90), 0.5*pi), "deg2rad failed");
+    static_assert(almost_equal(deg2rad(270), 1.5*pi), "deg2rad failed");
 
-    static_assert(almost_equal(rad2deg(0.0), 0.0), "rad2deg) failed");
+    static_assert(almost_equal(rad2deg(0.0), 0.0), "rad2deg failed");
+    static_assert(almost_equal(rad2deg(pi), 180), "rad2deg failed");
+    static_assert(almost_equal(rad2deg(2*pi), 360), "rad2deg failed");
+    static_assert(almost_equal(rad2deg(-pi/2), -90), "rad2deg failed");
+    static_assert(almost_equal(rad2deg(-pi/4), -45), "rad2deg failed");
     /// TASK: Write at least 4 additional tests for rad2deg. Include at least one negative angle
     /// HINT: It helps to use angles where there is a simple known formula (e.g., 30 degrees)
 
     static_assert(almost_equal(normalize_angle(0.0), 0.0), "norm_angle failed");
-    static_assert(almost_equal(normalize_angle(std::numbers::pi), std::numbers::pi), "norm_angle failed");
-    static_assert(almost_equal(normalize_angle(-std::numbers::pi), std::numbers::pi), "norm_angle failed");
-    // / Task: Write at least 8 additional tests for normalize_angle. This function is absolutely critical, so you want to get it right!
-    // / Include at least once case  where the angle > 5.0*pi and one where the angle < -5.0*pi
-    // / Also include: pi, -pi, -pi/4.0, 3*pi/2, and -5*pi/2.
-
-
+    static_assert(almost_equal(normalize_angle(pi), pi), "norm_angle failed");
+    static_assert(almost_equal(normalize_angle(-pi), pi), "norm_angle failed");
+    static_assert(almost_equal(normalize_angle(-pi/4), -pi/4), "norm_angle failed");
+    static_assert(almost_equal(normalize_angle(3*pi/2), -pi/2), "norm_angle failed");
+    static_assert(almost_equal(normalize_angle(-5*pi/2), -pi/2), "norm_angle failed");
+    static_assert(almost_equal(normalize_angle(-7*pi), pi), "norm_angle failed");
+    static_assert(almost_equal(normalize_angle(-8*pi), 0), "norm_angle failed");
+    static_assert(almost_equal(normalize_angle(7*pi), pi), "norm_angle failed");
+    static_assert(almost_equal(normalize_angle(10*pi), 0), "norm_angle failed");
+    static_assert(almost_equal(normalize_angle(pi/12), pi/12), "norm_angle failed");
 }
 #endif
