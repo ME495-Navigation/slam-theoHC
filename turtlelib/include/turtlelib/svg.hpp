5b474f94f3c7643fe930716d6085cc836ea16773
theoHC <theo_coulson@me.com>
2026-01-20 17:29:47 -0600

    Points now associated with frames

--- turtlelib/include/turtlelib/svg.hpp
+++ turtlelib/include/turtlelib/svg.hpp
@@ -1,22 +1,57 @@
 #pragma once
 #include <turtlelib/se2d.hpp>
 #include <vector>
+#include <string>
 
 /// \file
 /// \brief Helper for visualizing geometry as a .svg
 
 namespace turtlelib{
-    /// \brief A 
+
+    struct dispPoint{
+        Point2D point;
+        std::string color;
+        int frameID;
+    };
+
+    struct dispFrame{
+        Transform2D transform;
+        std::string name;
+    };
+
+    /// \brief A class for constructing an svg-based visualization of turtlelib geometry functions.
     class Svg{
         public:
+        /// \brief Default constructor which adds a frame at the origin.
+        Svg();
+
         float xsize = 8.5;
         float ysize = 11;
+        float dpi = 96;
 
+        /// \brief Writes all stored geometry as an svg to cout
         void write();
 
-        void addPoint(Point2D newpoint);
+        /// \brief Add a Point2D object to the geometric representation
+        /// \param newpoint the point to be added
+        /// \param color the name of the color of the point
+        /// \param frameID the index of the frame in which this point is represented (e.g. the first frame added)
+        void addPoint(Point2D newpoint, std::string color, unsigned int frameID);
+
+        /// @brief Add a frame object to the geometric representation
+        /// @param transform The transform representing the frame, relative to the origin
+        /// @param name The display name of the frame
+        void addFrame(Transform2D transform, std::string name);
 
         private:
-        std::vector<Point2D> points;
+        std::vector<dispPoint> points;
+
+        std::vector<dispFrame> frames;
+
+        Transform2D CenterOfPage = Transform2D(.5 * Vector2D(xsize, ysize));
+
+        void drawFrame(dispFrame frame);
+
+        void drawPoint(dispPoint point);
     };
 }
--- turtlelib/src/svg.cpp
+++ turtlelib/src/svg.cpp
@@ -1,18 +1,86 @@
 #include <turtlelib/svg.hpp>
 #include <iostream>
 
-void turtlelib::Svg::write(){
+turtlelib::Svg::Svg(){
+    addFrame(Transform2D(), "a");
+}
 
-    std::cout << std::format("<svg width=\"{}in\" height=\"{}in\" viewBox=\"0 0 816.000000 1056.000000\" xmlns=\"http://www.w3.org/2000/svg\">", xsize, ysize);
+void turtlelib::Svg::write()
+{
+
+    std::cout << std::format("<svg width=\"{}in\" height=\"{}in\" viewBox=\"0 0 {} {}\" xmlns=\"http://www.w3.org/2000/svg\">\n",
+        xsize, ysize, xsize * dpi, ysize * dpi);
+
+    std::cout << 
+        "<defs>\n"
+            "<marker style=\"overflow:visible\" id=\"Arrow1Sstart\" refX=\"0.0\" refY=\"0.0\" orient=\"auto\">\n"
+                "<path transform=\"scale(0.2) translate(6,0)\" \n"
+                "style=\"fill-rule:evenodd;fill:context-stroke;stroke:context-stroke;stroke-width:1.0pt\" \n"
+                "d=\"M 0.0,0.0 L 5.0,-5.0 L -12.5,0.0 L 5.0,5.0 L 0.0,0.0 z \"/>\n"
+            "</marker>\n"
+        "</defs>\n";
 
     //Draw points
-    for(Point2D point : points){
-        std::cout << std::format("<circle cx=\"{}\" cy=\"{}\" r=\"3\" stroke=\"purple\" fill=\"purple\" stroke-width=\"1\"/>", point.x, point.y);
+    for(dispPoint point : points){
+        drawPoint(point);
     }
 
     std::cout << "</svg>";
 }
 
-void turtlelib::Svg::addPoint(Point2D newpoint){
-    points.push_back(newpoint);
+void turtlelib::Svg::addPoint(Point2D newpoint, std::string color, unsigned int frameID){
+    if(frameID >= frames.size()) {
+        return;
+    }
+
+    dispPoint point = dispPoint();
+    
+    point.point = newpoint;
+    point.color = color;
+    point.frameID = frameID;
+
+    points.push_back(point);
+}
+
+
+
+void turtlelib::Svg::addFrame(Transform2D frame, std::string name){
+    dispFrame newframe = dispFrame();
+    newframe.transform = frame;
+    newframe.name = name;
+
+    frames.push_back(newframe);
+}
+
+void turtlelib::Svg::drawFrame(dispFrame frame){
+    Transform2D drawtrans = CenterOfPage * frame.transform;
+
+    Point2D origin = drawtrans(Point2D(0,0));
+    Point2D xhead = drawtrans(Point2D(1,0));
+    Point2D yhead = drawtrans(Point2D(0,1));
+
+    std::cout << "<g>";
+
+    std::cout << std::format(
+        "<line x1=\"{}\" y1=\"{}\" x2=\"{}\" y2=\"{}\" stroke=\"red\" stroke-width=\"2\" marker-end=\"url(#Arrow1Sstart)\"/>",
+        origin.x * dpi, origin.y * dpi, xhead.x * dpi, xhead.y * dpi);
+
+    std::cout << std::format(
+        "<line x1=\"{}\" y1=\"{}\" x2=\"{}\" y2=\"{}\" stroke=\"green\" stroke-width=\"2\" marker-end=\"url(#Arrow1Sstart)\"/>",
+        origin.x * dpi, origin.y * dpi, yhead.x * dpi, yhead.y * dpi);
+    
+    std::cout << std::format(
+        "<text x=\"{}\" y=\"{}\" fill=\"black\" font-size=\"12\">{}</text>",
+        origin.x * dpi + 5, origin.y * dpi - 5, frame.name);
+
+    std::cout << "</g>";
+}
+
+void turtlelib::Svg::drawPoint(dispPoint point){
+    Transform2D drawtrans = CenterOfPage * frames.at(point.frameID).transform;
+
+    Point2D cpoint = drawtrans(point.point);
+
+    std::cout << std::format("<circle cx=\"{}\" cy=\"{}\" r=\"3\" stroke=\"{}\" fill=\"{}\" stroke-width=\"1\"/>", 
+        cpoint.x * dpi, cpoint.y * dpi, point.color, point.color);
 }
